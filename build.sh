f61d0347c6c3bc2908fa202e30e7342fb74560b4
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-03 10:56:45 -0700

    add clean

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 export CC=clang
 
 
-echo "Building BitcoinMW"
-
-./scripts/secp256k1zkp.sh || exit 1;
+if [ "clean" = "$1" ]; then
+	echo "Cleaning"
+        cd secp256k1-zkp
+        make mostlyclean-compile
+        cd ..
+        rm -rf .obj/* libtest.a bin/* rust/test_deps/*/target
+else
+	echo "Building BitcoinMW"
+	./scripts/secp256k1zkp.sh || exit 1;
+fi
