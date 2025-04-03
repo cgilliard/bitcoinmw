0410fdf827a7e4cfe94d52a2159f2fedc0fb6e5f
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-03 12:01:00 -0700

    sleep

--- rust/ffi.rs
+++ rust/ffi.rs
@@ -1,3 +1,5 @@
+#[allow(dead_code)]
 extern "C" {
     pub fn write(fd: i32, s: *const u8, len: i32);
+    pub fn sleep(millis: u64);
 }
--- rust/real_main.rs
+++ rust/real_main.rs
@@ -1,9 +1,10 @@
-use ffi::write;
+use ffi::{sleep, write};
 
 #[no_mangle]
 pub extern "C" fn real_main(_argc: i32, _argv: *const *const u8) -> i32 {
     unsafe {
         write(2, "Running bitcoinmw...\n".as_ptr(), 21);
+        sleep(60);
     }
     0
 }
