eeada1503feec60d469d2e7e5bc49ae68e978645
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-03 14:48:06 -0700

    exit correctly

--- rust/real_main.rs
+++ rust/real_main.rs
@@ -1,10 +1,9 @@
-use ffi::{sleep, write};
+use ffi::write;
 
 #[no_mangle]
 pub extern "C" fn real_main(_argc: i32, _argv: *const *const u8) -> i32 {
     unsafe {
         write(2, "Running bitcoinmw...\n".as_ptr(), 21);
-        sleep(60);
     }
     0
 }
