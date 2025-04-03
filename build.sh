b11663b603ac8fc9aad4915ad3d699b0533057cb
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-03 11:05:29 -0700

    build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export CC=clang
+export CCFLAGS=-O3
 
 
 if [ "clean" = "$1" ]; then
@@ -8,8 +9,18 @@ if [ "clean" = "$1" ]; then
         cd secp256k1-zkp
         make mostlyclean-compile
         cd ..
-        rm -rf .obj/* libtest.a bin/* rust/test_deps/*/target
+        rm -rf .obj/* libtest.a bin/* 
 else
 	echo "Building BitcoinMW"
 	./scripts/secp256k1zkp.sh || exit 1;
+	cd c
+        for file in *.c
+        do
+                if [ ! -e ../.obj/${file%.c}.o ] || [ ${file} -nt ../.obj/${file%.c}.o ]; then
+                        echo "${CC} ${CCFLAGS} -o ../.obj/${file%.c}.o -c -Ic ${file}";
+                        ${CC} ${CCFLAGS} -o ../.obj/${file%.c}.o -c -Ic ${file} || exit 1;
+                fi
+        done
+	cd ..
+	${CC} ${CCFLAGS} -o bin/bmw .obj/*.o -L.obj -lsecp256k1 || exit 1;
 fi
--- c/main.c
+++ c/main.c
@@ -1,2 +1,8 @@
+#include <stdio.h>
+
 extern int real_main(int argc, char *argv[]);
+int real_main(int argc, char **argv) {
+	printf("bitcoinmw...\n");
+	return 0;
+}
 int main(int argc, char *argv[]) { return real_main(argc, argv); }
