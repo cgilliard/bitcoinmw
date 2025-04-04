c2c13347c323de5c425f027657959075fb3ea1ce
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-03 21:07:36 -0700

    rustfmt

--- rust/real_main.rs
+++ rust/real_main.rs
@@ -1,15 +1,15 @@
 #[no_mangle]
 pub extern "C" fn real_main(_argc: i32, _argv: *const *const u8) -> i32 {
-    0
+	0
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
-    use core::ptr::null;
+	use super::*;
+	use core::ptr::null;
 
-    #[test]
-    fn test_real_main() {
-        assert_eq!(real_main(0, null()), 0);
-    }
+	#[test]
+	fn test_real_main() {
+		assert_eq!(real_main(0, null()), 0);
+	}
 }
