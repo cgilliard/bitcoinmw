57ec62d00f902ebaeab65e7d866d4994c3a114a6
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-03 20:26:40 -0700

    coverage

--- rust/real_main.rs
+++ rust/real_main.rs
@@ -1,9 +1,15 @@
-use ffi::write;
-
 #[no_mangle]
 pub extern "C" fn real_main(_argc: i32, _argv: *const *const u8) -> i32 {
-    unsafe {
-        write(2, "Running bitcoinmw...\n".as_ptr(), 21);
-    }
     0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use core::ptr::null;
+
+    #[test]
+    fn test_real_main() {
+        assert_eq!(real_main(0, null()), 0);
+    }
 }
