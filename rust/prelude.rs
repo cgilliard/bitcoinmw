3215c6f69af56ed493800b59297acb9283661eff
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-05 16:04:43 -0700

    rc

--- rust/prelude.rs
+++ rust/prelude.rs
@@ -1,4 +1,5 @@
 // Internal
+pub use std::boxed::Box;
 pub use std::clone::TryClone;
 pub use std::error::{Error, ErrorKind, ErrorKind::*};
 pub use std::ptr::Ptr;
@@ -9,4 +10,5 @@ pub use core::cmp::PartialEq;
 pub use core::fmt::{Debug, Error as FmtError, Formatter};
 pub use core::marker::{Copy, Sized};
 pub use core::ops::Drop;
+pub use core::option::{Option, Option::None, Option::Some};
 pub use core::result::{Result, Result::Err, Result::Ok};
--- rust/std/mod.rs
+++ rust/std/mod.rs
@@ -1,5 +1,9 @@
+#[macro_use]
+pub mod macros;
+
 pub mod boxed;
 pub mod clone;
 pub mod error;
 pub mod misc;
 pub mod ptr;
+pub mod rc;
