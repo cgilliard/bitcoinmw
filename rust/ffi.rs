64912449d4818170f472487a2fda5afa36751407
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-05 11:32:07 -0700

    ptr

--- rust/ffi.rs
+++ rust/ffi.rs
@@ -5,5 +5,14 @@ extern "C" {
 	pub fn alloc(bytes: usize) -> *const u8;
 	pub fn release(ptr: *const u8);
 	pub fn resize(ptr: *const u8, bytes: usize) -> *const u8;
+
+	// sys
+	pub fn write(fd: i32, buf: *const u8, len: usize) -> i64;
+	pub fn ptr_add(p: *mut u8, v: i64);
+	pub fn exit(code: i32);
 	pub fn getalloccount() -> usize;
+
+	// misc
+	pub fn sleep_millis(millis: u64) -> i32;
+	pub fn f64_to_str(d: f64, buf: *mut u8, capacity: u64) -> i32;
 }
--- rust/prelude.rs
+++ rust/prelude.rs
@@ -3,4 +3,5 @@ pub use std::error::{Error, ErrorKind, ErrorKind::*};
 
 // External
 pub use core::fmt::{Debug, Error as FmtError, Formatter};
+pub use core::ops::Drop;
 pub use core::result::{Result, Result::Err, Result::Ok};
