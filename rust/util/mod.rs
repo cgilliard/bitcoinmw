844bae164e000e8ae529f80794f83549d21f584a
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-20 01:43:45 -0700

    use hashtable in real_main

--- rust/util/mod.rs
+++ rust/util/mod.rs
@@ -2,7 +2,8 @@ mod hashtable;
 mod murmur;
 mod rbtree;
 
-pub use util::hashtable::{Hashtable, Murmur3Hasher};
+pub use util::hashtable::{Murmur3Hasher, Node};
 pub use util::murmur::hash128::{murmurhash3_x64_128, Hasher128};
 pub use util::murmur::hash32::{murmurhash3_x86_32, Hasher32};
 pub use util::rbtree::RbTree;
+pub type Hashtable<V> = crate::util::hashtable::Hashtable<V, Murmur3Hasher>;
