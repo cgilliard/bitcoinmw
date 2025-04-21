17c506774742ec0bc66ff4906a62c0221d584d05
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-21 00:55:47 -0700

    move to sha384

--- c/bip52.c
+++ c/bip52.c
@@ -52,38 +52,3 @@ void heavyhash(const uint16_t matrix[64][64], void* pdata, size_t pdata_len,
 	memcpy(output, temp_output, 32);  // Truncate final output to 32 bytes
 }
 
-/*
-void heavyhash(const uint16_t matrix[64][64], void* pdata, size_t pdata_len,
-	       void* output) {
-	uint8_t hash_first[32] __attribute__((aligned(32)));
-	uint8_t hash_second[32] __attribute__((aligned(32)));
-	uint8_t hash_xored[32] __attribute__((aligned(32)));
-
-	uint16_t vector[64] __attribute__((aligned(64)));
-	uint16_t product[64] __attribute__((aligned(64)));
-
-	sha3_384(hash_first, pdata, pdata_len);
-
-	for (int i = 0; i < 32; ++i) {
-		vector[2 * i] = (hash_first[i] >> 4);
-		vector[2 * i + 1] = hash_first[i] & 0xF;
-	}
-
-	for (int i = 0; i < 64; ++i) {
-		uint16_t sum = 0;
-		for (int j = 0; j < 64; ++j) {
-			sum += matrix[i][j] * vector[j];
-		}
-		product[i] = (sum >> 10);
-	}
-
-	for (int i = 0; i < 32; ++i) {
-		hash_second[i] = (product[2 * i] << 4) | (product[2 * i + 1]);
-	}
-
-	for (int i = 0; i < 32; ++i) {
-		hash_xored[i] = hash_first[i] ^ hash_second[i];
-	}
-	sha3_384(output, hash_xored, 32);
-}
-*/
