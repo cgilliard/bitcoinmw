b25a2c5551c51abcd06003cd0a644d3540c56ce0
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-04-22 21:59:35 -0700

    simple macro expansion

--- macros/lib.rs
+++ macros/lib.rs
@@ -3,6 +3,9 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(Dummy)]
-pub fn derive_my_clone(_input: TokenStream) -> TokenStream {
-	TokenStream::new()
+pub fn derive_dummy(_input: TokenStream) -> TokenStream {
+	// just hard code a simple text to implement partial equal for our type
+	"impl PartialEq for MyStruct { fn eq(&self, _other: &MyStruct) -> bool { false } }"
+		.parse()
+		.expect("Failed to parse token stream")
 }
