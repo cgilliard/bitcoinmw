9f189af3a92a19e997fc325efa255e343d6e3ee4
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-04 11:22:08 -0700

    use platform independent values

--- rust/bmw/net/constants.rs
+++ rust/bmw/net/constants.rs
@@ -11,16 +11,9 @@ pub const ERROR_FCNTL: i32 = -7;
 pub const ERROR_GETSOCKNAME: i32 = -10;
 pub const ERROR_EAGAIN: i32 = -11;
 
-#[cfg(target_os = "linux")]
-pub const EVENT_SIZE: usize = 12;
-#[cfg(any(
-	target_os = "macos",
-	target_os = "freebsd",
-	target_os = "openbsd",
-	target_os = "netbsd"
-))]
+// linux is 12, macos is 32. We use 32 to be platform independent in rust code.
+// It's ok because only 128 events in the array (additional 2.5 kb for linux).
 pub const EVENT_SIZE: usize = 32;
-
 pub const EVH_MAX_EVENTS: usize = 128;
 pub const EVH_MAX_BYTES_PER_READ: usize = 16 * 1024;
 
